8dcc47e03eccb5d1f8c29bbf1601753a427df8dc
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2021-03-03 07:57:59 +0100

    Edit processOrder method in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -83,6 +83,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
     initAccordion() {
       const thisProduct = this;
@@ -142,8 +143,10 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if (formData[paramId] && formData[paramId].includes(optionId)) {
+          if (optionSelected) {
             // check if the option is not default
             if (!option.default == true) {
               // add option price to price variable
@@ -156,9 +159,16 @@
               price -= option.price;
             }
           }
+          if (optionImage !== null) {
+            if (optionSelected) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
+          // update calculated price in the HTML
+          thisProduct.priceElem.innerHTML = price;
         }
-        // update calculated price in the HTML
-        thisProduct.priceElem.innerHTML = price;
       }
     }
   }
